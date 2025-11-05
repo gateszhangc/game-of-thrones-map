e984719233308c448c58be0973e7ab3713988848
gateszhangc <gateszhang92@gmail.com>
2025-11-05 11:20:49 +0800

    Remove ignoreBuildErrors - TypeScript errors are now fixed

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     optimizePackageImports: ["node-html-parser"]
-  },
-  // Disable Tailwind CSS warnings
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      config.optimization.minimizer.forEach((minimizer) => {
-        if (minimizer.constructor.name === 'TerserPlugin') {
-          minimizer.options.terserOptions.compress.warnings = false;
-        }
-      });
-    }
-    return config;
   }
 };
 
